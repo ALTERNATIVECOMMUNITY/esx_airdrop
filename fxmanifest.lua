cbbcc40996a6252d7e62499df13a361da5476656
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:47:40 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'karpo#1943'
+
+client_scripts { "ck/cl_main.lua", "config.lua" }
+server_scripts { "@mysql-async/lib/MySQL.lua", "sk/sv_main.lua", "config.lua" }
