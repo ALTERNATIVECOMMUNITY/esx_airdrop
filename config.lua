2eb3dc6689bc62e51d7982c99e6bce84d473d3e1
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:46:58 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,109 @@
+Config = {}
+Config.kuinusein = 120 --kuin usein airdrop, minuutteina
+
+Config.Gangs = {
+  "police",
+  "ambulance"
+}
+
+Config.requiredModels = {
+    "p_cargo_chute_s", 
+    "ex_prop_adv_case_sm", 
+    "cuban800", 
+    "s_m_m_pilot_02",
+    "prop_box_wood02a_pu"
+}
+
+
+Config.Items = {
+	"luotiliivi", 
+	"lipas",
+	"silu",
+	"superskini"
+}
+
+Config.Weapons = {
+	{label = "SMG", weapon = "WEAPON_SMG"},
+    {label = "50KAL", weapon = "WEAPON_PISTOL50"},
+    {label = "MUSKETTI", weapon = "WEAPON_MUSKET"},
+    {label = "AK-47", weapon = "WEAPON_ASSAULTRIFLE"},
+    {label = "COMBAT-PDW", weapon = "WEAPON_COMBATPDW"},
+    {label = "MICRO-SMG", weapon = "WEAPON_MICROSMG"},
+    {label = "REVOLVER", weapon = "WEAPON_REVOLVER"},
+    {label = "BULLPUPSHOTGUN", weapon = "WEAPON_BULLPUPSHOTGUN"},
+}
+
+Config.Locations = {
+    [1] = {
+        coords = vector3(1622.6, 4544.36, 43.16),
+        kuiniso = 250,
+        kuinylos = 90,
+        menossa = false,
+        collectattu = false
+    },
+    [2] = {
+        coords = vector3(2101.66, 2332.08, 94.29),
+        kuiniso = 250,
+        kuinylos = 138,
+        menossa = false,
+        collectattu = false
+    },
+    [3] = {
+        coords = vector3(2739.98, 1460.35, 30.13),
+        kuiniso = 350,
+        kuinylos = 75,
+        menossa = false,
+        collectattu = false
+    },
+    [4] = {
+        coords = vector3(1056.08, -3041.71, 5.9),
+        kuiniso = 250,
+        kuinylos = 50,
+        menossa = false,
+        collectattu = false
+    },
+    [5] = {
+        coords = vector3(770.44, 1282.48, 360.3),
+        kuiniso = 250,
+        kuinylos = 400,
+        menossa = false,
+        collectattu = false
+    },
+    [6] = {
+        coords = vector3(-1892.32, 2039.1, 140.87),
+        kuiniso = 250,
+        kuinylos = 185,
+        menossa = false,
+        collectattu = false
+    },
+    [7] = {
+        coords = vector3(1535.68, -2160.89, 77.68),
+        kuiniso = 250,
+        kuinylos = 135,
+        menossa = false,
+        collectattu = false
+    },
+    [8] = {
+        coords = vector3(-1804.27, -1189.41, 13.02),
+        kuiniso = 250,
+        kuinylos = 80,
+        menossa = false,
+        collectattu = false
+    },
+    [9] = {
+        coords = vector3(-2296.2, 380.78, 174.47),
+        kuiniso = 225,
+        kuinylos = 215,
+        menossa = false,
+        collectattu = false
+    },
+    [10] = {
+        coords = vector3(-415.74, 1157.53, 325.87),
+        kuiniso = 250,
+        kuinylos = 370,
+        menossa = false,
+        collectattu = false
+    }
+}
+
+
